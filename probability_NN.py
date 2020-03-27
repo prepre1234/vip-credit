a096e6b15d03ef5139dbbce08554c6aa85e3481c
DY <dywechat@163.com>
2020-03-27 22:12:36 +0800

    add dataset_test

--- probability_NN.py
+++ probability_NN.py
@@ -3,15 +3,17 @@ from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
+from torch.utils.data import random_split
 
 import argparse
 import numpy as np
 import pandas as pd
 from datetime import datetime
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--bs', type=int, default=32, help='size of the batches')
+parser.add_argument('--bs', type=int, default=64, help='size of the batches')
 parser.add_argument('--epo', type=int, default=1, help='size of epoches')
 parser.add_argument('--dim', type=int, default=10, help='dim of data')
 opt = parser.parse_args()
@@ -69,8 +71,9 @@ criterion = nn.BCELoss()
 network = Network()
 
 # Optimizer
-optimizer = torch.optim.Adam(
-    network.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizer = torch.optim.SGD(network.parameters(), lr=1e-3, momentum=0.9)
+# optimizer = torch.optim.Adam(
+#     network.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
 cuda = True if torch.cuda.is_available() else False
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
@@ -79,18 +82,27 @@ if cuda:
     network.cuda()
     criterion.cuda()
 
-data = pd.read_csv('./dataDump/balance_data.csv').iloc[:, 1:]
+# data = pd.read_csv('./dataDump/balance_data.csv').iloc[:, 1:]
+data_df = pd.read_csv('./data/cs-training.csv').iloc[:, 1:]
+data_df = data_df.dropna()
+
 # original data -> 0 for good guys, 1 for bad guys,so revert labels
-labels = 1-np.array(data.iloc[:, 0]).reshape(data.shape[0], -1)
-data_train = np.array(data.iloc[:, 1:])
+data_df.iloc[:, 0] = 1-data_df.iloc[:, 0]
+
+labels = np.array(data_df.iloc[:, 0]).reshape(data_df.shape[0], -1)
+data = np.array(data_df.iloc[:, 1:])
+dataset_train, dataset_test, label_train, label_test = train_test_split(
+    data, labels, test_size=0.3)
 
 dataloader = DataLoader(
-    dataset(data_train, labels),
+    dataset(dataset_train, label_train),
     batch_size=opt.bs,
     shuffle=True,
     drop_last=True
 )
 
+dataset_test = Variable(Tensor(dataset_test).type(Tensor))
+label_test = Variable(Tensor(label_test).type(Tensor))
 
 print('training...')
 begin = datetime.now()
@@ -103,6 +115,7 @@ for epoch in range(opt.epo):
         train = Variable(batch["data"].type(Tensor))
         label = Variable(batch["labels"].type(Tensor))
 
+        network.train()
         optimizer.zero_grad()
 
         possibility = network(train)
@@ -111,22 +124,21 @@ for epoch in range(opt.epo):
         loss.backward()
         optimizer.step()
 
+        network.eval()
+        # dataset_test
+        pr = network(dataset_test)
+        test_loss = criterion(pr, label_test)
+
+        # accuracy
+        predict_labels = torch.ge(pr, 0.5).float()
+        correct = torch.eq(predict_labels, label_test).sum()
+        acc = correct.item()/predict_labels.shape[0]
+
         # print log
         print(
-            "[Epoch %d/%d] [Batch %d/%d] [Loss: %f]"
-            % (epoch, opt.epo, i, len(dataloader), loss.item())
+            "[Epoch %d/%d] [Batch %d/%d] [trainLoss: %f] [testLoss: %f] [acc: %f]"
+            % (epoch, opt.epo, i, len(dataloader), loss.item(), test_loss.item(), acc)
         )
 
 end = datetime.now()
 print("total time:", (end-begin).seconds)
-
-
-# -------------save probability--------------
-'''test with train dataset'''
-
-print('save probablility...')
-
-network.eval()
-probability = pd.DataFrame(
-    network(Variable(Tensor(data_train).type(Tensor))).detach().numpy())
-probability.to_csv('./dataDump/probability_NN.csv')
