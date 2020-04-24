ac05a34c6b8bd4dd99fa004e50f2567d77986f58
DY <dywechat@163.com>
2020-04-24 22:32:15 +0800

    add region

--- probability_NN.py
+++ probability_NN.py
@@ -1,4 +1,4 @@
-from get_data import computeTime
+from utils import computeTime
 from utils import readDataFromMysql
 import torch
 from torch import nn
@@ -54,17 +54,23 @@ else:
 
 if not pub:
     ##############################
-    sql = 'select zjc_supplier_param.attach_count,zjc_supplier.type,zjc_supplier.fund,zjc_supplier_param.service_level_average,zjc_supplier_param.instock_service_average,zjc_supplier_param.instock_product_average,zjc_supplier_param.instock_deliverspeed_average,zjc_supplier_param.iswin_count,zjc_supplier_param.price_level_average,zjc_supplier_param.tender_count,zjc_supplier_param.semester_tender_count,zjc_supplier_param.bidconcern_count,zjc_supplier_param.semester_bidconcern_count,zjc_supplier_param.login_days,zjc_supplier_param.semester_login_days,zjc_supplier_param.integrity_count,zjc_supplier_param.contract_rate,zjc_supplier_param.instock_honesty_average,zjc_supplier.regchecktime,zjc_supplier.createtime from zjc_supplier INNER JOIN zjc_supplier_param on zjc_supplier.id=zjc_supplier_param.supplier_id where zjc_supplier.state=2;'
+    sql = 'select zjc_supplier.name,zjc_supplier.province,zjc_supplier_param.attach_count,zjc_supplier.type,zjc_supplier.fund,zjc_supplier_param.service_level_average,zjc_supplier_param.instock_service_average,zjc_supplier_param.instock_product_average,zjc_supplier_param.instock_deliverspeed_average,zjc_supplier_param.iswin_count,zjc_supplier_param.price_level_average,zjc_supplier_param.tender_count,zjc_supplier_param.semester_tender_count,zjc_supplier_param.bidconcern_count,zjc_supplier_param.semester_bidconcern_count,zjc_supplier_param.login_days,zjc_supplier_param.semester_login_days,zjc_supplier_param.integrity_count,zjc_supplier_param.contract_rate,zjc_supplier_param.instock_honesty_average,zjc_supplier.regchecktime,zjc_supplier.createtime from zjc_supplier INNER JOIN zjc_supplier_param on zjc_supplier.id=zjc_supplier_param.supplier_id where zjc_supplier.state=2;'
+    # pre_data:将要预测的供应商（所有供应商，包括训练用的正负样本）
     pre_data = readDataFromMysql(sql)
+    # name_region：前两列是供应商的名字和地区，保存留到后面使用
+    name_region = pre_data[[0, 1]]
+    # pre_data:分离name_region后的剩余列数据
+    pre_data = pre_data.iloc[:, 2:]
     total = pre_data.shape[0]
 
-    time = computeTime(pre_data[[18, 19]])
-    # pre_data = pd.concat(
-    #     [pre_data.iloc[:, [2, 7, 9, 10, 11, 12, 13, 14, 15]], time], axis=1)
+    # 最后两列是注册和审核时间，用于计算已经成为平台用户的时长
+    time = computeTime(pre_data.iloc[:, 18:])
+    # 将注册和审核时间替换为时长
     pre_data = pd.concat([pre_data.iloc[:, 0:18], time], axis=1)
     pre_data = pre_data.astype('float32')
     pre_data = np.array(pre_data)
 
+    # 标准化处理
     scaler = preprocessing.StandardScaler().fit(pre_data)
     pre_data = scaler.transform(pre_data)
 
@@ -155,9 +161,9 @@ network = Network()
 network.apply(weights_init)
 
 # Optimizer
-optimizer = torch.optim.SGD(network.parameters(), lr=lr, momentum=0.9)
-# optimizer = torch.optim.Adam(
-#     network.parameters(), lr=lr, betas=(beta1, beta2))
+# optimizer = torch.optim.SGD(network.parameters(), lr=lr, momentum=0.9)
+optimizer = torch.optim.Adam(
+    network.parameters(), lr=lr, betas=(beta1, beta2))
 
 # lr_decay
 scheduler = torch.optim.lr_scheduler.LambdaLR(
@@ -173,16 +179,19 @@ if cuda:
 if pub:
     data_df = pd.read_csv('./data/cs-training.csv').iloc[:, 1:]
 else:
+    # data_df：目前的VIP正负样本
     data_df = pd.read_csv('./dataDump/vip_data.csv').iloc[:, 1:]
     # data_df = data_df.iloc[:, [0, 3, 8, 10, 11, 12, 13, 14, 15, 16, 19]]
 
 data_df = data_df.dropna()
 
-# 实验时取得数据比例
+# 实验时按比例ds取部分数据集作为训练集+测试集，如果用全部数据，则ds=1
 data_df = data_df.iloc[0:int(data_df.shape[0]*ds)]
 
-labels = np.array(data_df.iloc[:, 0]).reshape(data_df.shape[0], -1)
-data = np.array(data_df.iloc[:, 1:])
+# data_df[pd.DataFrame(data_df.iloc[:, 0]).duplicated()] #查找正负样本重叠部分（数据给的有问题）
+name_set = set(data_df.iloc[:, 0])
+labels = np.array(data_df.iloc[:, 1]).reshape(data_df.shape[0], -1)
+data = np.array(data_df.iloc[:, 2:])
 
 if pub:
     scaler = preprocessing.StandardScaler().fit(data)
@@ -261,11 +270,70 @@ if not pub:
     # prediction
     network.eval()
     pre_data = Variable(Tensor(pre_data).type(Tensor))
-    pre = network(pre_data)
+    # pre_result预测的概率
+    pre_result = network(pre_data)
 
-    will = torch.ge(pre, 0.5).float().sum()
-    no = torch.lt(pre, 0.4).float().sum()
+    will = torch.ge(pre_result, 0.5).float().sum()
+    no = torch.lt(pre_result, 0.4).float().sum()
 
+    # 会充值/0.4-0.5二次充值/不会充值
     print('>=0.5:\t', will)
     print('>=0.4&&<0.5\t:', total-will-no)
     print('<0.4:\t', no)
+
+    pre_result = pd.DataFrame(pre_result.detach().numpy())
+    # pre_result更新为：名字、地区、概率
+    pre_result = pd.concat([name_region, pre_result], axis=1)
+    # pre_positive预测会充值的供应商
+    pre_positive = pre_result[pre_result.iloc[:, 2] >= 0.5]
+    pre_positive.columns = ['name', 'province', 'prob']
+
+    # 去除正负样本中的供应商
+    tmp = []
+    for row in pre_positive.itertuples():
+        name = getattr(row, 'name')
+        if name not in name_set:
+            tmp.append(row)
+
+    pre_positive = pd.DataFrame(tmp)
+    pre_positive.sort_values('prob', ascending=False, inplace=True,)
+    print('最终预测VIP充值的有：', len(pre_positive))
+
+    Yangtze_River_Delta = set({'上海市', '江苏省', '浙江省', '安徽省'})
+    # top50/长三角/其他
+    top = pre_positive.iloc[0:50]
+    top_Yangtze_River_Delta = []
+    top_other = []
+    for row in top.itertuples():
+        province = getattr(row, 'province')
+        if province in Yangtze_River_Delta:
+            top_Yangtze_River_Delta.append(row)
+        else:
+            top_other.append(row)
+    top_Yangtze_River_Delta = pd.DataFrame(top_Yangtze_River_Delta)
+    top_other = pd.DataFrame(top_other)
+    print('Top50 长三角地区：', len(top_Yangtze_River_Delta))
+    print('Top50 其余地区：', len(top_other))
+
+    # remain/长三角/其他
+    remain = pre_positive.iloc[50:]
+    remain_Yangtze_River_Delta = []
+    remain_other = []
+    for row in remain.itertuples():
+        province = getattr(row, 'province')
+        if province in Yangtze_River_Delta:
+            remain_Yangtze_River_Delta.append(row)
+        else:
+            remain_other.append(row)
+    remain_Yangtze_River_Delta = pd.DataFrame(remain_Yangtze_River_Delta)
+    remain_other = pd.DataFrame(remain_other)
+    print('50之后 长三角地区：', len(remain_Yangtze_River_Delta))
+    print('50之后 其余地区：', len(remain_other))
+
+    # save results
+    top_Yangtze_River_Delta.iloc[:, 2:4].to_csv(
+        './dataResult/前50_长三角地区.csv', index=False)
+    top_other.iloc[:, 2:4].to_csv('./dataResult/前50_其余地区.csv', index=False)
+    remain_Yangtze_River_Delta.iloc[:, 2:4].to_csv(
+        './dataResult/50之后_长三角地区.csv', index=False)
+    remain_other.iloc[:, 2:4].to_csv('./dataResult/50之后_其余地区.csv', index=False)
